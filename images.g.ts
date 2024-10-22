3005bb33b363ed18cea7531e81a08851f3a5e8b6
Oliver Mohr <94675539+designermohr@users.noreply.github.com>
2024-10-22 22:00:09 +0200

    Irgendwas nach der Änderung mit JavScript im ersten Block

--- images.g.ts
+++ images.g.ts
@@ -1,115 +1,16 @@
-namespace myImages {
-    helpers._registerFactory("image", function(name: string) {
-        switch(helpers.stringTrim(name)) {
-            case "brick_hell_01":
-                return img`
-                cccccccccccccccb
-                ddddddddddbbdd
-                bdbbddbbbbbbcdd
-                bbbbbbbbbbbbcdd
-                bbbbbbbbbbbbcdd
-                bbbbbbbbbbbcdb
-                bbbbbbbbbb7cdb
-                eeeeeeeeeeeeeee
-                cccccbccccccccb
-                dddbddddddddddd
-                bbbbcddbbdbbddd
-                bbbbcdbbbbbbbbd
-                bbbbcdbbbbbbbbd
-                bbbbcdbbbbbbbbb
-                bbb7cdbbbbbbbbb
-                e7eeebeeeeeeeeb
-                `;
-        }
-        return null;
-    });
-}
-
 // Automatisch generierter Code. Nicht bearbeiten.
 namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image2":
-            case "Test-Bild0":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . 2 2 2 2 2 2 2 . . . . . . 
-. . . 2 . . . . . 2 . . . . . . 
-. . . 2 . . . . 2 2 . . . . . . 
-. . . 2 . . 2 2 . . . . . . . . 
-. . . 2 . . . 2 2 . . . . . . . 
-. . . 2 . . . . 2 . . . . . . . 
-. . . 2 . . . . 2 . . . . . . . 
-. . . 2 . . . . 2 . . . . . . . 
-. . . 2 2 2 2 2 2 . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`;
-            case "image1":
-            case "Test-Bild2":return img`
-f . . . . . . . . . . . . . . f 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . 2 2 2 2 2 2 2 . . . . . . 
-. . . 2 . . . . . 2 . . . . . . 
-. . . 2 . . . . 2 2 . . . . . . 
-. . . 2 . . 2 2 . . . . . . . . 
-. . . 2 . . . 2 2 . . . . . . . 
-. . . 2 . . . . 2 . . . . . . . 
-. . . 2 . . . . 2 . . . . . . . 
-. . . 2 . . . . 2 . . . . . . . 
-. . . 2 2 2 2 2 2 . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-f . . . . . . . . . . . . . . f 
-`;
+
         }
         return null;
     })
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "Test-Animation":
-            case "anim1":return [img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . 7 7 . . . . . . . . . 
-. . . . . 7 7 7 . . . . . . . . 
-. . . . . 7 . 7 7 . . . . . . . 
-. . . . . 7 . . 7 . . . . . . . 
-. . . . . 7 . . . 7 . . . . . . 
-. . . . 7 . 7 7 7 7 . . . . . . 
-. . . 7 7 . . . . . 7 . . . . . 
-. . . 7 . . . . . . . 7 . . . . 
-. . . 7 . . . . . . . 7 . . . . 
-. . . 7 . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . 7 . . . . . . . . 
-. . . . . . . 7 7 . . . . . . . 
-. . . . . . 7 . 7 . . . . . . . 
-. . . . . . 7 . . 7 . . . . . . 
-. . . . . 7 . . . 7 . . . . . . 
-. . . . . 7 . . . . 7 . . . . . 
-. . . . 7 7 7 7 7 . 7 . . . . . 
-. . . 7 . . . . . . 7 . . . . . 
-. . . 7 . . . . . . 7 . . . . . 
-. . 7 . . . . . . . . 7 . . . . 
-. . 7 . . . . . . . . 7 . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`];
+
         }
         return null;
     })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,43 +1,5 @@
 // Automatisch generierter Code. Nicht bearbeiten.
 namespace myTiles {
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const transparency16 = image.ofBuffer(hex``);
-
-    helpers._registerFactory("tilemap", function(name: string) {
-        switch(helpers.stringTrim(name)) {
-            case "Test-Level":
-            case "Test-Level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000002000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,myTiles.tile1,sprites.castle.tilePath5], TileScale.Sixteen);
-        }
-        return null;
-    })
-
-    helpers._registerFactory("tile", function(name: string) {
-        switch(helpers.stringTrim(name)) {
-            case "Test-Kachel":
-            case "tile1":return tile1;
-            case "transparency16":return transparency16;
-        }
-        return null;
-    })
 
 }
 // Automatisch generierter Code. Nicht bearbeiten.
